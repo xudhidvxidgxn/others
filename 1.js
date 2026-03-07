5e20f8fb727cbf266e628881c5df322dad62aca4
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-03-08 01:27:40 +0900

    afsd

--- 1.js
+++ 1.js
@@ -1,13 +1,35 @@
 // @ts-check
-const input_num = document.querySelector("#input-num");
-const input_title = document.querySelector("#input-title");
+const input_num = /** @type {HTMLInputElement} */ (document.querySelector("#input-num"));
+const input_title = /** @type {HTMLInputElement} */ (document.querySelector("#input-title"));
+const display = /** @type {HTMLDivElement} */ (document.querySelector("#display"));
 
 let content_data = [
-    { no: 75596, title: "숲의 아이", link: "" },
+    { no: 75596, title: "숲의 아이", link: "d" },
     { no: 51581, title: "Become", link: "" },
 ];
 
-/** 화면 로딩 함수 */
-function render() {
-    
+/** 화면 로딩 함수 
+ * @param {string|number|null} [filter] - 필터입니다. 
+*/
+function render(filter = null) {
+    display.innerHTML = "";
+    const data = filter ? content_data.filter((value) => `${value["no"]}`.includes(`${filter}`) || value["title"].includes(`${filter}`)) : content_data;
+    const insert_html = data.map((value, index) => {
+        const { no, title, link } = value;
+        const link_html = link.trim() !== "" ? `<div class="content-link">${link}</div>` : "";
+
+        return `<div class="content" data-no="${no}" data-title="${no}">
+            <div class="content-no">${no}</div>
+            <div class="content-title">${title}</div>
+            ${link_html}
+            <div class="btn-bar">
+                <button class="edit-btn">수정</button>
+                <button class="delete-btn">삭제</button>
+            </div>
+        </div>`;
+    }).join("\n");
+
+    display.insertAdjacentHTML("beforeend", insert_html);
 }
+
+render();
